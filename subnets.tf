e1da21c44eb87d290d239b13957b874f67ee3771
MalickReborn <malickguessennd@gmail.com>
2023-08-30 15:55:46 +0000

    integration eks tag into subnets

--- subnets.tf
+++ subnets.tf
@@ -2,9 +2,12 @@ resource "aws_default_subnet" "public1" {
   vpc_id = aws_default_vpc.vpc_central.id
   availability_zone = "us-east-1a"
   cidr_block = "192.168.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "Default subnet for us-west-2a"
+    Name                        = "public-us-east-1a"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
   }
 }
 
@@ -12,9 +15,12 @@ resource "aws_default_subnet" "public2" {
   vpc_id = aws_default_vpc.vpc_central.id   
   availability_zone = "us-east-1b"
   cidr_block = "192.168.64.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "Default subnet for us-eat-1a"
+    Name                        = "public-us-east-1b"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
   }
 }
 
@@ -24,7 +30,9 @@ resource "aws_default_subnet" "public1" {
   cidr_block = "192.168.128.0/24"
 
   tags = {
-    Name = "Default subnet for us-west-1a"
+    Name                        = "private-us-east-1a"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
   }
 }
 
@@ -34,6 +42,8 @@ resource "aws_default_subnet" "public2" {
   cidr_block = "192.168.192.0/24"
 
   tags = {
-    Name = "Default subnet for us-west-1b"
+    Name                        = "private-us-east-1b"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
   }
 }
